99f880599a9cf62e78c9b63e1ed7b714c1f4d500
samir karki <samirkarki123@gmail.com>
2026-01-19 21:08:50 +0545

    first commit

---
+++ 1. Digital Clock/main.js
@@ -0,0 +1,31 @@
+const hourEl = document.getElementById("hours");
+const minuteEl = document.getElementById("minutes");
+const secondsEl = document.getElementById("seconds");
+const ampmEl = document.getElementById("ampm");
+
+function upadteClock(){
+    let h = new Date().getHours();
+    let m = new Date().getMinutes();
+    let s = new Date().getSeconds();
+    let ampm = "AM";
+
+    if(h > 12){
+    h = h - 12;
+    ampm = "PM";
+    }
+
+    h = h < 10 ? "0" + h : h;
+    m = m < 10 ? "0" + m : m;
+    s = s < 10 ? "0" + s : s;
+    
+
+    hourEl.innerText = h;
+    minuteEl.innerText = m;
+    secondsEl.innerText = s;
+    ampmEl.innerText = ampm;
+    setTimeout(() => {
+        upadteClock();
+    }, 1000);
+}
+
+upadteClock();
