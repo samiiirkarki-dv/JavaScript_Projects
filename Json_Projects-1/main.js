38a0e15af4f849f059a58cd302458db996cfe246
samir karki <samirkarki123@gmail.com>
2026-01-25 22:06:10 +0545

    updated project-4

--- Json_Projects-1/main.js
+++ Json_Projects-1/main.js
@@ -1,71 +1,4 @@
-// // DOM ELEMENT
 
-// const studentForm = document.getElementById("studentForm");
-// const studentContainer = document.querySelector(".students");
-// const nameInput = studentForm["name"];
-// const ageInput = studentForm["age"];
-// const rollInput = studentForm["roll"];
-
-// /*
-// {
-// name: string,
-// age: number,
-// roll: number,
-// }
-// */
-
-// const students = JSON.parse(localStorage.getItem("students")) ||[];
-
-// const addStudent = (name, age, roll) => {
-//   students.push({
-//     name,
-//     age,
-//     roll,
-//   })
-
-//   localStorage.setItem("students", JSON.stringify(students));
-//   return {name, age, roll};
-// };
-
-// const createStudentElement = ({ name, age, roll }) => {
-//   // Create Element
-
-//   const studentDiv = document.createElement("div");
-//   const studentName = document.createElement("h1");
-//   const studentAge = document.createElement("p");
-//   const studentRoll = document.createElement("P");
-
-//   // Fill the content
-
-//   studentName.innerText = `Student name: ${name}`;
-//   studentAge.innerText = `Age: ${age}`;
-//   studentRoll.innerText = `Roll: ${roll}`;
-
-//   // Add to the DOM
-
-//   studentDiv.append(studentName, studentAge, studentRoll);
-//   studentContainer.appendChild(studentDiv);
-//   studentContainer.style.display = students.length === 0 ? "none" : "flex";
-// };
-
-// studentContainer.style.display = students.length === 0 ? "none" : "flex";
-// students.forEach(createStudentElement);
-
-// studentForm.onsubmit = (event) => {
-//   event.preventDefault();
-
-//   const newStudent = addStudent(
-//     nameInput.value,
-//     nameInput.value,
-//     nameInput.value,
-//   );
-
-//   createStudentElement(newStudent);
-
-//   nameInput.value = "";
-//   ageInput.value = "";
-//   rollInput.value = "";
-// };
 
 const studentForm = document.getElementById("studentForm");
 const studentContainer = document.querySelector(".students");
@@ -73,47 +6,46 @@ const nameInput = studentForm["name"];
 const ageInput = studentForm["age"];
 const rollInput = studentForm["roll"];
 
-/*
+/* 
 {
-name: "string",
+name: string,
 age: number,
 roll: number,
-
 }
 
 */
 
-const students = JSON.parse(localStorage.getItem("students")) || [];
- 
+const students = JSON.parse(localStorage.getItem("students")) ||[];
+
 
 const addStudent = (name, age, roll) => {
   students.push({
-    name,
+    name, 
     age,
     roll,
   });
 
-  localStorage.setItem("students", JSON.stringify(students));
-  return {name, age, roll};
+  localStorage.setItem("stuents", JSON.stringify(students));
+ return {name, age, roll};
+
 };
 
 const createStudentElement = ({ name, age, roll }) => {
-  // Create the Element
-
+  // Create Element
   const studentDiv = document.createElement("div");
   const studentName = document.createElement("h1");
-  const studentAge = document.createElement("p");
-  const studentRoll = document.createElement("p");
+  const studentAge = document.createElement("P");
+  const studenttRoll = document.createElement("p");
 
   // Fill the content
 
   studentName.innerText = `Student name: ${name}`;
   studentAge.innerText = `Age: ${age}`;
-  studentRoll.innerText = `Roll: ${roll}`;
+  studenttRoll.innerText = `Roll no: ${roll}`;
 
-  //Add to the DOM
+  // Add to the DOM
 
-  studentDiv.append(studentName, studentAge, studentRoll);
+  studentDiv.append(studentName, studentAge, studenttRoll);
   studentContainer.appendChild(studentDiv);
 };
 
@@ -129,8 +61,11 @@ studentForm.onsubmit = (event) => {
   );
 
   createStudentElement(newStudent);
-
-  nameInput.value = ""
-    ageInput.value = ""
-    rollInput.value =  ""
+  nameInput.value = "";
+  ageInput.value = "";
+  rollInput.value = "";
 };
+
+
+
+
