acba865fd3433c38587586af884003f78d5b2d29
samir karki <samirkarki123@gmail.com>
2026-02-01 22:03:50 +0545

    project-12

---
+++ Calculator/main.js
@@ -0,0 +1,19 @@
+// DOM ELEMENTS
+
+const display = document.getElementById("display");
+
+function buttonsClick(input) {
+  display.value += input;
+}
+
+function calculate() {
+  try {
+    display.value = eval(display.value);
+  } catch (error) {
+    display.value = "Error";
+  }
+}
+
+function clearDisplay() {
+    display.value = "";
+}
