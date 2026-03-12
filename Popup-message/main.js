44016958a2d552d438fd80766b47a8f73b0dfeef
samir karki <samirkarki123@gmail.com>
2026-03-12 19:43:11 +0545

    final soluton

---
+++ Popup-message/main.js
@@ -0,0 +1,9 @@
+let message = document.getElementById("message");
+
+function openpopUp(){
+    message.classList.add("open-popup");
+}
+
+function closePopup(){
+    message.classList.remove("open-popup");
+}
