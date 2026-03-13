5739676fd6a49e8140a267759cadd027e6874554
samir karki <samirkarki123@gmail.com>
2026-03-13 23:07:12 +0545

    up-js

--- Music Player/script.js
+++ Music Player/script.js
@@ -1,39 +1,3 @@
-// let progress = document.getElementById("progress");
-// let song = document.getElementById("song");
-// let ctrlIcon = document.getElementById("ctrlIcon");
-
-// song.onloadedmetadata = () =>{
-//     progress.max = song.duration;
-//     progress.value = song.curentTime;
-// }
-
-// function playPause(){
-//     if(ctrlIcon.classList.contains("fa-pause")){
-//         song.pause();
-//         ctrlIcon.classList.remove("fa-pause");
-//         ctrlIcon.classList.add("fa-play");
-
-//     } else{
-//         song.play();
-//         ctrlIcon.classList.remove("fa-play");
-//         ctrlIcon.classList.add("fa-pause");
-
-//     }
-// }
-
-// if(song.play()){
-//     setInterval(() => {
-//         progress.value = song.currentTime;
-//     }, 500);
-// }
-
-// progress.onchange = () => {
-//     song.play();
-//     song.currentTime = progress.value;
-//     ctrlIcon.classList.add("fa-pause");
-//         ctrlIcon.classList.remove("fa-play");
-// }
-
 let song = document.getElementById("song");
 let progress = document.getElementById("progress");
 let ctrlIcon = document.getElementById("ctrlIcon");
@@ -55,16 +19,15 @@ function playPause() {
   }
 }
 
-
-if(song.play()){
-    setInterval(() => {
-        progress.value = song.currentTime;
-    }, 500);
+if (song.play()) {
+  setInterval(() => {
+    progress.value = song.currentTime;
+  }, 500);
 }
 
 progress.onchange = () => {
-song.play();
-song.currentTime = progress.value;
-ctrlIcon.classList.add("fa-pause");
-ctrlIcon.classList.remove("fa-play");
-}
+  song.play();
+  song.currentTime = progress.value;
+  ctrlIcon.classList.add("fa-pause");
+  ctrlIcon.classList.remove("fa-play");
+};
